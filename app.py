2a1b97c06b31c87d9362c995262b26fb641587e3
Ebenezer Tseh <ebenezertseh@dhcp-10-250-41-6.harvard.edu>
2024-01-09 15:44:18 -0500

    okay

--- app.py
+++ app.py
@@ -3,18 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://kngpxbrvuuxqie:59500891407391f315c217ae7af60713cb5fca7e74e4239291ae0cbd0cedebcc@ec2-54-164-138-85.compute-1.amazonaws.com:5432/d8aple15ssibb5'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://kngpxbrvuuxqie:59500891407391f315c217ae7af60713cb5fca7e74e4239291ae0cbd0cedebcc@ec2-54-164-138-85.compute-1.amazonaws.com:5432/d8aple15ssibb5'
 db = SQLAlchemy(app)
 app.app_context().push()
-
-# Model definition
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
-
-    def __repr__(self):
-        return '<Task %r>' % self.id
 
 @app.route('/', methods = ["POST", "GET"])
 def index():
